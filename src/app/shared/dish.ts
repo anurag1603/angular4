3e706ec119d0cdfb1fa8366b4876cc4897d45e01
anurag gupta <anurag@Samads-MacBook-Pro.local>
2018-03-20 14:55:26 +0530

    Single Page Applications Part 1

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -2,11 +2,13 @@ import { Comment } from './comment';
 
 
 export class Dish {
+    id: number;
     name: string;
     image: string;
     category: string;
     label: string;
     price: string;
+    featured: Boolean;
     description: string;
     comments: Comment[];
 }
