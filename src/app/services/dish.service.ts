134f7ed1c7b1ed985df07a1f6ffbebe8251496cb
anurag gupta <anurag@Samads-MacBook-Pro.local>
2018-04-07 21:38:38 +0530

    promise part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { resolve } from 'url';
 
 
 @Injectable()
@@ -9,14 +10,26 @@ export class DishService {
   constructor() { }
 
   getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise(resolve => {
+      // for latency of 2 secs
+      setTimeout(() => resolve(DISHES), 2000);
+    });
+    // return Promise.resolve(DISHES);
   }
 
   getDish(id: number): Promise<Dish> {
-    return Promise.resolve(DISHES.filter(dish => (dish.id === id))[0]);
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      // for latency of 2 secs
+      setTimeout(() => resolve(DISHES.filter(dish => (dish.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter(dish => dish.featured)[0]);
+    // tslint:disable-next-line:no-shadowed-variable
+    return new Promise((resolve, reject) => {
+      // for latency of 2 secs
+      setTimeout(() => resolve(DISHES.filter(dish => dish.featured)[0]), 2000);
+    });
   }
-}
